73269acf5e8ad1063b8696a982c636ddb48a0946
Arshia Naseri <arshianaseri@Arshias-MacBook-Pro.local>
2024-02-04 11:53:29 -0500

    new 1

--- src/components/WelcomePage components/joinPage.js
+++ src/components/WelcomePage components/joinPage.js
@@ -31,6 +31,8 @@ const JoinPage = () => {
         // Fake server requests to wake up servers from hibernation 
         fetch(process.env.REACT_APP_WAKEUP_URL);
         io(process.env.REACT_APP_SOCKET_SERVER);
+        console.log(LOGIN_API_URL)
+        console.log(SIGNUP_API_URL)
     },[])
     const loginFormSubmit = (e) =>{
         setIsLoading(true);
