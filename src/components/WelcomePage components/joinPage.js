0e67ab44f4e2bf870ab86ea3f7edca3b9f855abf
Arshia Naseri <arshianaseri@Arshias-MacBook-Pro.local>
2024-02-07 19:18:35 -0500

    fake request removed

--- src/components/WelcomePage components/joinPage.js
+++ src/components/WelcomePage components/joinPage.js
@@ -31,8 +31,6 @@ const JoinPage = () => {
         // Fake server requests to wake up servers from hibernation 
         // fetch(process.env.REACT_APP_WAKEUP_URL);
         // io(process.env.REACT_APP_SOCKET_SERVER);
-        console.log(LOGIN_API_URL)
-        console.log(SIGNUP_API_URL)
     },[])
     const loginFormSubmit = (e) =>{
         setIsLoading(true);
@@ -190,5 +188,5 @@ const JoinPage = () => {
         </>
     )
 }
-// ✔
+
 export default JoinPage;
